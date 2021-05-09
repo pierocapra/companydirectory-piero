e9d324f6fba56a3aaa3f00349a9a5fd3b6b994fd
Piero Capra <petecapra07@gmail.com>
2021-05-09 21:00:33 +0200

    main desktop layout

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+$(document).ready(function () {
+  $("#btnDep").on("click", () => {
+    $("#depSection").css("transform", "translateX(0)");
+    $("#locSection").css("transform", "translateX(-110%)");
+    $("#btnDep").css("width", "40px");
+    $("#btnLoc").css("width", "30px");
+  });
+  $("#btnLoc").on("click", () => {
+    $("#depSection").css("transform", "translateX(-110%)");
+    $("#locSection").css("transform", "translateX(0)");
+    $("#btnDep").css("width", "30px");
+    $("#btnLoc").css("width", "40px");
+  });
+});
